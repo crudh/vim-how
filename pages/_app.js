33131727763b88b45d976c22eb3084c00befdc89
Christian Rudh <christian@rudh.se>
2020-08-30 22:42:36 +0200

    Initial setup

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/index.css";
+import * as React from "react";
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>vim.how</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>vim.how</main>
+
+      <footer>footer</footer>
+    </div>
+  );
+};
+
+export default Home;
