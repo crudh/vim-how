7aa7964db340b31c8fa1a78a72e71298cff29180
Christian Rudh <christian@rudh.se>
2020-10-12 17:17:26 +0200

    Added styled 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { LinkButton } from "../components/link-button";
+
+const NotFoundPage: React.FC = () => (
+  <div className="grid place-items-center h-full">
+    <div className="flex flex-col items-center">
+      <h3>404 &#124; Nothing found</h3>
+      <div className="pt-8">
+        <LinkButton label="Go to start" to="/" />
+      </div>
+    </div>
+  </div>
+);
+
+export default NotFoundPage;
