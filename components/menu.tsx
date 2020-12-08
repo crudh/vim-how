009e4cc6004f683db248bfa5a98682d4a013faa0
Christian Rudh <christian@rudh.se>
2020-12-08 19:24:44 +0100

    Updated next, next-pwa and some dev deps

--- components/menu.tsx
+++ components/menu.tsx
@@ -25,7 +25,11 @@ const MenuTogglePath: FC<SVGMotionProps<SVGPathElement>> = (props) => (
 );
 
 const MenuToggle: FC<{ onToggle: () => void }> = ({ onToggle }) => (
-  <button className="absolute top-5 left-5" onClick={onToggle}>
+  <button
+    className="absolute top-5 left-5"
+    onClick={onToggle}
+    aria-label="Menu"
+  >
     <svg width="30" height="30" viewBox="0 0 20 20">
       <MenuTogglePath
         variants={{
