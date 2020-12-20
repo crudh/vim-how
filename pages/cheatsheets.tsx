77ac90570318e84c7fbe69d7d8bb41db5eebd9cf
Christian Rudh <christian@rudh.se>
2020-12-20 16:09:09 +0100

    Added content to the cheat sheets page

--- pages/cheatsheets.tsx
+++ pages/cheatsheets.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Head from "next/head";
 import { LinkButton } from "../components/link-button";
+import { ContentLinkItem } from "../components/content-link-item";
 
 const CheatSheetsPage: FC = () => (
   <>
@@ -10,7 +11,37 @@ const CheatSheetsPage: FC = () => (
     <div className="flex flex-col items-center pb-8">
       <LinkButton label="Back" to="/" />
     </div>
-    <h2>Cheat Sheets</h2>
+    <h2 className="pb-8">Cheat sheets</h2>
+    <div className="max-w-xl text-center pb-8">
+      Cheat sheets gives a condensend overview of commands and are excellent for
+      printing.
+    </div>
+    <div className="max-w-xl">
+      <ContentLinkItem title="vim.rtorr.com" link="https://vim.rtorr.com/">
+        An excellent cheat sheet that aims to be one of the most accessible vim
+        guides available. They made sure to support mobile, desktop and many
+        languages.
+      </ContentLinkItem>
+      <ContentLinkItem
+        title="hackjutsu / vim-cheatsheet"
+        link="https://github.com/hackjutsu/vim-cheatsheet"
+      >
+        A cheatsheet summarized from personal experience and other online
+        tutorials.
+      </ContentLinkItem>
+      <ContentLinkItem
+        title="vimhelp.org quick reference"
+        link="https://vimhelp.org/quickref.txt.html"
+      >
+        A quick help reference from the vim reference manual.
+      </ContentLinkItem>
+      <ContentLinkItem
+        title="Basic PDF cheat sheet"
+        link="https://www.cs.cmu.edu/~15131/f17/topics/vim/vim-cheatsheet.pdf"
+      >
+        A good printable cheat sheet as PDF with the basic commands.
+      </ContentLinkItem>
+    </div>
   </>
 );
 
--- pages/editors.tsx
+++ pages/editors.tsx
@@ -1,20 +1,7 @@
 import { FC } from "react";
 import Head from "next/head";
 import { LinkButton } from "../components/link-button";
-
-const Editor: FC<{ title: string; link: string }> = ({
-  title,
-  link,
-  children,
-}) => (
-  <div className="transition duration-300 p-4 border-gray-900 border-2 rounded-lg mb-4 hover:border-gray-500">
-    <h3>{title}</h3>
-    <div className="pt-4 pb-4">{children}</div>
-    <a className="font-bold" href="https://onivim.io/">
-      {link}
-    </a>
-  </div>
-);
+import { ContentLinkItem } from "../components/content-link-item";
 
 const EditorsPage: FC = () => (
   <>
@@ -25,28 +12,32 @@ const EditorsPage: FC = () => (
       <LinkButton label="Back" to="/" />
     </div>
     <h2 className="pb-8">Editors</h2>
+    <div className="max-w-xl text-center pb-8">
+      Editors that, either by default or by extensions, provide a Vim-like
+      editing experience.
+    </div>
     <div className="max-w-xl">
-      <Editor title="Onivim 2" link="https://onivim.io/">
+      <ContentLinkItem title="Onivim 2" link="https://onivim.io/">
         Fast, native, cross-platform combining Vim-style modal editing with the
         aesthetics and language features of modern editors. Aims to support all
         Visual Studio Code extensions.
-      </Editor>
-      <Editor
+      </ContentLinkItem>
+      <ContentLinkItem
         title="Visual Studio Code"
         link="https://marketplace.visualstudio.com/items?itemName=vscodevim.vim"
       >
         Vim emulation for Visual Studio Code exists as an installable extension.
-      </Editor>
-      <Editor
+      </ContentLinkItem>
+      <ContentLinkItem
         title="IntelliJ platform"
         link="https://plugins.jetbrains.com/plugin/164-ideavim"
       >
         IdeaVim is a Vim emulation plugin for IntelliJ Platform-based IDEs like
         WebStorm and IntelliJ.
-      </Editor>
-      <Editor title="Neovim" link="https://neovim.io/">
+      </ContentLinkItem>
+      <ContentLinkItem title="Neovim" link="https://neovim.io/">
         Hyperextensible Vim-based text editor.
-      </Editor>
+      </ContentLinkItem>
     </div>
   </>
 );
