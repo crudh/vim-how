c728a74c620473c8af590af34d4462dcec934398
Christian Rudh <christian@rudh.se>
2020-12-21 14:23:22 +0100

    Fix design for small screens

--- components/content-link-item.tsx
+++ components/content-link-item.tsx
@@ -5,11 +5,13 @@ export const ContentLinkItem: FC<{ title: string; link: string }> = ({
   link,
   children,
 }) => (
-  <div className="transition duration-300 p-4 border-gray-900 border-2 rounded-lg mb-4 hover:border-gray-500">
-    <h3>{title}</h3>
-    <div className="pt-4 pb-4">{children}</div>
-    <a className="font-bold" href={link}>
-      {link}
-    </a>
+  <div className="p-2">
+    <div className="transition duration-300 p-4 border-gray-900 border-2 rounded-lg mb-4 hover:border-gray-500">
+      <h3 className="break-all font-bold">{title}</h3>
+      <div className="pt-4 pb-4">{children}</div>
+      <a className="font-bold break-all" href={link}>
+        {link}
+      </a>
+    </div>
   </div>
 );
