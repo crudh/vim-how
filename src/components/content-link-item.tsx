8c87dd7b1725ded8004f7e5e62a12a21bc60449b
Christian Rudh <christian@rudh.se>
2021-01-02 17:05:10 +0100

    Use gray for descriptions and content text

--- src/components/content-link-item.tsx
+++ src/components/content-link-item.tsx
@@ -8,7 +8,7 @@ export const ContentLinkItem: FC<{ title: string; link: string }> = ({
   <div className="p-2">
     <div className="transition duration-300 p-4 border-gray-900 border-2 rounded-lg mb-4 hover:border-gray-500">
       <h3 className="break-all font-bold">{title}</h3>
-      <div className="pt-4 pb-4">{children}</div>
+      <div className="pt-4 pb-4 text-gray-400">{children}</div>
       <a className="font-bold break-all" href={link}>
         {link}
       </a>
