bb5e06a2c7c4ebe8cc96769dc7c4762f429dd215
Christian Rudh <christian@rudh.se>
2021-01-03 20:34:11 +0100

    Add categories as menu items and add some spacing

---
+++ src/data/menu-items.ts
@@ -0,0 +1,35 @@
+import { categoryList } from "./categories";
+
+export type MenuItem = {
+  name: string;
+  url: string;
+  isExternal?: boolean;
+  isSubItem?: boolean;
+  spaceAbove?: boolean;
+};
+
+export const menuItems: MenuItem[] = [
+  {
+    name: "Start",
+    url: "/",
+  },
+  ...categoryList.map((category) => ({
+    name: category.name,
+    url: `/category/${category.id}`,
+    isSubItem: true,
+  })),
+  {
+    name: "Cheat sheets",
+    url: "/cheatsheets",
+    spaceAbove: true,
+  },
+  { name: "Tutorials", url: "/tutorials" },
+  { name: "Editors", url: "/editors" },
+  {
+    name: "GitHub",
+    url: "https://github.com/crudh/vim-how",
+    isExternal: true,
+    spaceAbove: true,
+  },
+  { name: "Contact", url: "https://crudh.se", isExternal: true },
+];
