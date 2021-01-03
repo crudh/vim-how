1b879bfe201b8e47b555b2066d85b83cf6537c93
Christian Rudh <christian@rudh.se>
2021-01-03 21:11:54 +0100

    Kill the code for using CMD as meta for Mac since it's wrong (#71)

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,6 +1,3 @@
 export const hasWindow = (): boolean => typeof window !== "undefined";
 
-export const isMac = (): boolean =>
-  hasWindow() && window.navigator.platform.toLowerCase().startsWith("mac");
-
 export const isOffLine = (): boolean => hasWindow() && !window.navigator.onLine;
