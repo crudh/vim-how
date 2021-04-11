f3e276082cc75b2e2a51678628de5ab2b2b9b46b
Christian Rudh <christian@rudh.se>
2021-04-11 22:01:35 +0200

    Start using webpack 5 (#83)

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 const nextConfig = {
+  future: {
+    webpack5: true,
+  },
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
